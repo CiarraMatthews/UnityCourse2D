85c26ae7da24a028a91a0301aa6baefba4c24578
CiarraMatthews <cdmatthews@ucdavis.edu>
2023-02-03 08:24:17 -0800

    Finished Driver script. Started Snowboarding project

--- DeliveryDriver/Assets/Delivery.cs
+++ DeliveryDriver/Assets/Delivery.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
 
+
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] float Delay = 0.5f;
     bool hasPackage = false;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch");
@@ -13,15 +23,18 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) {
         //if (the thin)
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package Picked Up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject, Delay);
         }
         else if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package Delivered");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
         else {
             Debug.Log("Where is the package??");
